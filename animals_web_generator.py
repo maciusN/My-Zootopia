c0f4c1c865050a84a5282dee9055060ce59cc6a7
Maciej <maciusngames@gmail.com>
2024-10-02 13:12:44 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,13 +1,15 @@
 import json
 
+
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
-  
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
 with open("animals_template.html", "r") as f:
-  html_content = f.read()
-  
+    html_content = f.read()
+
 
 animals_data = load_data('animals_data.json')
 
@@ -15,17 +17,20 @@ output = ""
 
 for animal in animals_data:
     output += '<li class="cards__item">'
-    output += f"Name: {animal['name']}</br>"
-    output += f"Diet: {animal['characteristics']['diet']}</br>"
-    output += f"Location: {animal['locations'][0]}</br>"
+    output += f'<div class="card__title">{animal['name']}</div></br>'
+    output += '<p class="card__text">'
+    output += f"<strong>Location:</strong> {animal['locations'][0]}</br>"
     try:
-      output += f"Type: {animal['characteristics']['type']}</br>"
+        output += f"<strong>Type:</strong> {animal['characteristics']['type']}</br>"
     except:
-      pass
+        pass
+    output += f"<strong>Diet:</strong> {
+        animal['characteristics']['diet']}</br>"
+    output += '</p>'
     output += '</li>'
 
 new_html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
 
 
 with open("animals.html", "w") as f:
-  f.write(new_html_content)
+    f.write(new_html_content)
