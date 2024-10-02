d569176f23df43d0b94073f5a6dcbf6cd5cd407d
Maciej <maciusngames@gmail.com>
2024-10-02 13:46:59 +0200

    finish

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,35 +2,67 @@ import json
 
 
 def load_data(file_path):
-    """ Loads a JSON file """
+    """
+    Loads a JSON file from the given file path.
+
+    Args:
+        file_path (str): The path to the JSON file.
+
+    Returns:
+        dict: The loaded JSON data as a dictionary.
+    """
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
-with open("animals_template.html", "r") as f:
-    html_content = f.read()
+def serialize_animal(animal_obj):
+    """
+    Serializes an animal dictionary into an HTML string.
 
+    Args:
+        animal (dict): The animal data.
 
-animals_data = load_data('animals_data.json')
-
-output = ""
-
-for animal in animals_data:
+    Returns:
+        str: The serialized HTML string for the animal.
+    """
+    output = ""
     output += '<li class="cards__item">'
-    output += f'<div class="card__title">{animal['name']}</div></br>'
+    output += f'<div class="card__title">{animal_obj['name']}</div></br>'
     output += '<p class="card__text">'
-    output += f"<strong>Location:</strong> {animal['locations'][0]}</br>"
+    output += f"<strong>Location:</strong> {animal_obj['locations'][0]}</br>"
     try:
-        output += f"<strong>Type:</strong> {animal['characteristics']['type']}</br>"
+        output += f"<strong>Type:</strong> {
+            animal_obj['characteristics']['type']}</br>"
     except:
         pass
     output += f"<strong>Diet:</strong> {
-        animal['characteristics']['diet']}</br>"
+        animal_obj['characteristics']['diet']}</br>"
     output += '</p>'
     output += '</li>'
+    return output
 
-new_html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+def main():
+    """
+    Main function to generate the animals HTML file.
+    It reads the template HTML, loads the animals data,
+    serializes each animal's information, and writes the
+    updated content to a new HTML file.
+    """
+    with open("animals_template.html", "r") as f:
+        html_content = f.read()
+
+    animals_data = load_data('animals_data.json')
+    output = ""
+
+    for animal_obj in animals_data:
+        output += serialize_animal(animal_obj)
+
+    new_html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+    with open("animals.html", "w") as f:
+        f.write(new_html_content)
 
 
-with open("animals.html", "w") as f:
-    f.write(new_html_content)
+if __name__ == "__main__":
+    main()
